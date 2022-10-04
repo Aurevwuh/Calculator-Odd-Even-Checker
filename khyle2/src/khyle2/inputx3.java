3b124cf905b5b7098abbc8e8924db585464aa1b0
Aurevwuh <114642446+Aurevwuh@users.noreply.github.com>
2022-10-03 21:56:13 -0700

    Add files via upload
    Version1

---
+++ khyle2/src/khyle2/inputx3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package khyle2;
+
+/**
+ *
+ * @author Reyes_CPE112
+ */
+class inputx3 {
+    
+}
